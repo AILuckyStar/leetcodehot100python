de9abb58bdab30ef82c12cc33532baeca885d45d
AILuckyStar <duansy@stu.pku.edu.cn>
2024-05-28 23:21:52 +0800

    Update 49. 字母异位词分组.py

--- 49. 字母异位词分组.py
+++ 49. 字母异位词分组.py
@@ -1,1 +1,14 @@
+import collections
 
+
+class Solution:
+    def groupAnagrams(self,strs):
+        mp = collections.defaultdict(list)
+        for st in strs:
+            key = "".join(sorted(st))
+            mp[key].append(st)
+        return list(mp.values())
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+sol = Solution()
+print(sol.groupAnagrams(strs))
