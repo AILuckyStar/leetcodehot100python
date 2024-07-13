aa7ecb59b43b01723a9ebad15aaacecb5f47ff2d
AILuckyStar <duansy@stu.pku.edu.cn>
2024-07-13 19:17:48 +0800

    Create 739. 每日温度.py

---
+++ 739. 每日温度.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self,temperatures):
+        l=len(temperatures)
+        ans = [0]*l
+        stk=[0]
+        for i in range(l):
+            if temperatures[i]<=temperatures[stk[-1]]:
+                stk.append(i)
+            else:
+                while stk and temperatures[i]>temperatures[stk[-1]]:
+                    ans[stk[-1]]=i-stk[-1]
+                    stk.pop()
+                stk.append(i)#无论栈空了，还是栈顶比当前大了
+        return ans
