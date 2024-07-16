248227c80fd28c80cd216a9de8a063eaa43cacef
AILuckyStar <duansy@stu.pku.edu.cn>
2024-07-16 20:56:41 +0800

    Create 300. 最长递增子序列.py

---
+++ 300. 最长递增子序列.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLIS(self,nums):
+        n=len(nums)
+        dp=[0]*n
+        dp[0]=1
+        res=1
+        for i in range(1,n):
+            dp[i]=1
+            for j in range(i):
+                if nums[i]>nums[j]:
+                    dp[i]=max(dp[i],dp[j]+1)
+            res=max(res,dp[i])
+        return res
